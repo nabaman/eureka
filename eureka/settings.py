ad72d50adab25326f05337bf525970270520eff5
naba <naba.alvian@gmail.com>
2021-02-07 19:32:05 +0700

    wew

--- eureka/settings.py
+++ eureka/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y2xw(=h03mnqwev**p8za*k#sg$$sn(9ammg!y94c2jaypgpgu'
 DEBUG_PROPAGATE_EXCEPTIONS = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
